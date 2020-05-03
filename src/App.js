a8a99fc1109b551a8420791927551b03f241b278
Tristan Malmenayde <tristanmalmenayde@hotmail.fr>
2020-05-03 19:07:14 +0200

    Firebase collection & authentfication /Sign Up component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndUpPage from './pages/sign-in-up/sign-in-up.component';
-import {auth} from './firebase/firebase.utils';
+
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -20,9 +21,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
-      this.setState({ currentUser: user });
-      
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+          console.log(this.state);
+        })
+      }
+      this.setState({currentUser: userAuth});
       console.log('auth success');
     })
   }
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
-import './sign-in.styles.scss';
-
 import FormInput from '../form-input/form-input.component';
 
 import CustomButton from '../custom-button/custom-button.component';
 
 import { signInWithGoogle } from '../../firebase/firebase.utils';
+
+import './sign-in.styles.scss';
+
 
 class SignIn extends React.Component {
     constructor(props){
