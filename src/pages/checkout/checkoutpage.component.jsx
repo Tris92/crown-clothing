18a8f352f36af9bfcdbb6a9e26fb649d3448e19a
Tristan Malmenayde <tristanmalmenayde@hotmail.fr>
2020-05-08 00:18:30 +0200

    added checkout page, checkout item component and updated cart to use selectors

---
+++ src/pages/checkout/checkoutpage.component.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
+import './checkoutpage.styles.scss';
+
+const CheckoutPage = ({cartItems, total}) => (
+    <div className='checkout-page'>
+        <div className='checkout-header'>
+            {/* HEADER  */}
+            <div className='header-block'>
+                <span>
+                    Product
+                </span>
+            </div>
+
+            <div className='header-block'>
+                <span>
+                    Description
+                </span>
+            </div>
+
+            <div className='header-block'>
+                <span>
+                    Quantity
+                </span>
+            </div>
+
+            <div className='header-block'>
+                <span>
+                    Price
+                </span>
+            </div>
+
+            <div className='header-block'>
+                <span>
+                    Remove
+                </span>
+            </div>
+            {/* END OF HEADER */}
+        </div>
+        {
+            cartItems.map(cartItem => 
+                (
+                    <CheckoutItem 
+                        cartItem={cartItem} 
+                        key={cartItem.id}
+                    />
+                )
+            )
+        }
+
+        <div className='total'>
+            <span>TOTAL: ${total}</span>
+        </div>
+    </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems, 
+    total: selectCartTotal
+}) 
+
+export default connect(mapStateToProps)(CheckoutPage);
