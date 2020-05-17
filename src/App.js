e0924c3069a770c2fc4466af3122bb8f7400bffe
Tristan Malmenayde <tristanmalmenayde@hotmail.fr>
2020-05-17 22:08:09 +0200

    created user and cart sagas for sign in, sign up and sign in with google

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
+import { checkUserSession } from './redux/user/user.actions'
 
 import Header from './components/header/header.component';
 
@@ -20,19 +21,8 @@ class App extends React.Component {
 
   componentDidMount() {
 
-    // this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-    //   if (userAuth) {
-    //     const userRef = await createUserProfileDocument(userAuth);
-
-    //     userRef.onSnapshot(snapShot => {
-    //       setCurrentUser({
-    //         id: snapShot.id,
-    //         ...snapShot.data()
-    //       });
-    //     });
-    //   }
-    //   setCurrentUser(userAuth);
-    // });
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
   componentWillUnmount() {
@@ -64,8 +54,12 @@ class App extends React.Component {
   }
 }
 
+const mapDispatchToProps = dispatch => ({
+  checkUserSession: () => dispatch(checkUserSession())
+});
+
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,8 +1,19 @@
 import UserActionTypes from './user.types';
 
+// FOR GOOGLE SIGN IN 
+
 export const googleSignInStart = () => ({
   type: UserActionTypes.GOOGLE_SIGN_IN_START
 });
+
+// FOR EMAIL AND PASSWORD 
+
+export const emailSignInStart = emailAndPassword => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_START, 
+  payload: emailAndPassword
+});
+
+// FOR BOTH GOOGLE & EMAIL/PSWRD 
 
 export const signInSuccess = user => ({
   type: UserActionTypes.SIGN_IN_SUCCESS,
@@ -14,10 +25,40 @@ export const signInFailure = error => ({
   payload: error
 });
 
-// FOR EMAIL AND PASSWORD 
+// FOR SESSION CHECK 
 
-export const emailSignInStart = emailAndPassword => ({
-  type: UserActionTypes.EMAIL_SIGN_IN_START, 
-  payload: emailAndPassword
+export const checkUserSession = () => ({
+  type: UserActionTypes.CHECK_USER_SESSION
 });
 
+// FOR SIGN-OUT 
+
+export const signOutStart = () => ({
+  type: UserActionTypes.SIGN_OUT_START
+});
+
+export const signOutSuccess = () => ({
+  type: UserActionTypes.SIGN_OUT_SUCCESS
+});
+
+export const signOutFailure = (error) => ({
+  type: UserActionTypes.SIGN_OUT_FAILURE,
+  payload: error
+});
+
+// FOR SIGN-UP 
+
+export const signUpStart = (userCredentials) => ({
+  type: UserActionTypes.SIGN_UP_START,
+  payload: userCredentials
+});
+
+export const signUpSuccess = ({user, additionalData}) => ({
+  type: UserActionTypes.SIGN_UP_SUCCESS, 
+  payload: {user, additionalData}
+});
+
+export const signUpFailure = error => ({
+  type: UserActionTypes.SIGN_UP_FAILURE, 
+  payload: error
+});
